8a8e7146accdba61585e2e3cb9b3cd366219ddb0
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-05-07 20:52:00 +0300

    task 6

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", () => {
+  const expectedLength = input.dataset.length;
+  const currentLength = input.value.length;
+
+  if (currentLength === Number(expectedLength)) {
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
+  }
+});

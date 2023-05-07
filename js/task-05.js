a1caf2d30c2fe0977ac19f03c922b66ff6a3a836
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-05-07 20:23:13 +0300

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (e) => {
+  output.textContent = e.currentTarget.value
+    ? e.currentTarget.value
+    : "Anonymous";
+});
